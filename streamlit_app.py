a0c4d99b3b34a88e4b3d23f0aac9c700cb1f1e3d
Chris Van Pelt <vanpelt@gmail.com>
2020-12-05 17:39:15 -0800

    Change docstring, render path

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,16 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# W&B Streamlit Demo
 """
 
 
-with st.echo(code_location='below'):
+with st.echo(code_location="below"):
     query_params = st.experimental_get_query_params()
     path = query_params.get("path", "wandb/dogs/3a1k7v1o")
 
-    api = wandb.Api()
-    run = api.run(path)
-    st.dataframe(run.history())
+    st.markdown("# Path\n`{}`".format(path))
+    # TODO: waiting for a way to get secrets
+    # api = wandb.Api()
+    # run = api.run(path)
+    # st.dataframe(run.history())
